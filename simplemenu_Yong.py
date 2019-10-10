03e72245a5225d343a117cd452f0d699da1dab32
longzeer <shao0015@algonquinlive.com>
2019-10-10 13:15:04 -0400

    Modified the menu for testing functions

--- simplemenu_Yong.py
+++ simplemenu_Yong.py
@@ -1,23 +1,44 @@
-from tkinter import *
-#from lab5_library_Yong import *
 
-def close():
-    exit()
+from lab5_library_Yong import *
+def simplemenu():
+    while True:
+        choice = '0'
+        while choice == '0':
+            print("Welcome! Please choose 1 of 5 choices to test the function.")
+            print("Choose 1 for testing Vertical line")
+            print("Choose 2 for testing Horizontal line")
+            print("Choose 3 for testing Staircase")
+            print("Choose 4 for testing Random pixel")
+            print("Choose 5 for testing Clear Backlight")
+            print("Choose 6 to exit python")
+            choice = input("Please choose one of the options: ")
+            if choice == "5":
+                print("Clear Backlight")
+                clearBacklight()
+            elif choice == "4":
+                print("Random pixel")
+                print("Ctrl+c to stop")
+                randompixel()
+            elif choice == "3":
+                print("Staircase")
+                a=int(input("The x is "))
+                b=int(input("The y is "))
+                c=int(input("The w is "))
+                d=int(input("The h is "))
+                staircase(a,b,c,d)
+            elif choice == "2":
+                print("Horizontal line")
+                e=int(input("Give me a Y: "))
+                horizontalline(e)
+            elif choice == "1":
+                print("Vertical line")
+                f = int(input("Give me a X: "))
+                verticalline(f)
+            elif choice == "6":
+                print('Goodbye')
+                exit()
+            else:
+                print("Please choose 1 to 5")
 
 
-window = Tk()
-menubar = Menu(window)
-
-filemenu = Menu(menubar,tearoff=0)
-filemenu.add_command(label="Vertical line")#,command=verticalline(float('Enter a x')))
-filemenu.add_command(label="Horizontal line")#,command=horizontalline(float('Enter a y')))
-filemenu.add_command(label="Staircase")
-filemenu.add_command(label="Random pixel")
-filemenu.add_command(label="Clear Backlight")
-filemenu.add_command(label="Close",command=close)
-
-menubar.add_cascade(label="Test Function",menu=filemenu)
-
-window.config(menu=menubar)
-window.mainloop()
-
+simplemenu()
